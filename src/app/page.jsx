63fc0ab142b1cf782ba106260d6840bf74277635
Vaibhavi Upreti <vaibhaviu.ind@gmail.com>
2024-06-29 23:48:33 +0530

    optimized code structure

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <div>helloo</div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black text-white">
+      <div>This is the homepage</div>
     </main>
   );
 }
